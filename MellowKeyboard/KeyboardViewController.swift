dcd258859e059e74bd49437a67840048f49a1f05
saumil <saumil.aggarwal@euphonyinc.com>
2016-09-11 18:50:37 -0400

    Mellow Keyboard IOS

---
+++ MellowKeyboard/KeyboardViewController.swift
@@ -0,0 +1,131 @@
+//
+//  KeyboardViewController.swift
+//  MellowKeyboard
+//
+//  Created by Test Euphony on 9/10/16.
+//  Copyright © 2016 EuphonyInc. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+
+    var dotButton: UIButton!
+    var dashButton: UIButton!
+    var deleteButton: UIButton!
+    var hideKeyboardButton: UIButton!
+    
+    var customInterface: UIView!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        let nib = UINib(nibName: "View", bundle: nil)
+        let objects = nib.instantiateWithOwner(self, options: nil)
+        customInterface = objects[0] as! UIView
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+    
+        // Add custom view sizing constraints here
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(customInterface)
+        
+        addKeyboardButtons()
+
+    }
+    
+    
+    func addKeyboardButtons() {
+  
+        
+        addNextKeyboardButton()
+     
+    }
+    
+    
+    
+    func addNextKeyboardButton() {
+        nextKeyboardButton = UIButton(type: .System) as UIButton
+        
+        nextKeyboardButton.setTitle(NSLocalizedString("🌐", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
+        nextKeyboardButton.sizeToFit()
+        nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+        
+        view.addSubview(nextKeyboardButton)
+        
+        
+        
+        let nextKeyboardButtonLeftSideConstraint = NSLayoutConstraint(item: nextKeyboardButton, attribute: .Left, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier: 1.0, constant: +10.0)
+        let nextKeyboardButtonBottomConstraint = NSLayoutConstraint(item:nextKeyboardButton, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: -10.0)
+        view.addConstraints([nextKeyboardButtonLeftSideConstraint, nextKeyboardButtonBottomConstraint])
+    }
+     
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+
+    override func textWillChange(textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+
+    override func textDidChange(textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+    
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
+            textColor = UIColor.whiteColor()
+        } else {
+            textColor = UIColor.blackColor()
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
+    }
+
+    
+    
+}
+
+class RoundButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.shadowColor = UIColor.cyanColor().CGColor
+            layer.shadowRadius = 3
+            layer.shadowOpacity = 1
+            layer.shadowOffset = CGSizeZero
+        }
+        
+    }
+    
+    
+   
+}
+
+class RoundLabel: UILabel {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+}
+
